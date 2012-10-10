526bb5f85a159b545d087a9de11204bf4de80817
drhodes <physci@gmail.com>
2012-10-10 12:50:41 -0400

    adds test maxprocs that doesn't work.

---
+++ maxprocs_test.go
@@ -0,0 +1,70 @@
+package pq
+
+import (
+	"log"
+	"database/sql"
+	"testing"
+	"math/rand"
+)
+
+const createperson = `
+create table person (
+        did SERIAL,
+        unique(did),
+
+        name varchar(255),
+        email varchar(255),
+        unique(email)
+
+        );
+`
+
+func TestMaxProcs(t *testing.T) {
+	db := openTestConn(t)
+	defer db.Close()
+
+    // Drop the table if it exists
+    _, err := db.Exec(`drop table if exists person cascade;`)
+    if err != nil {
+        log.Fatal(err, "\ncan't drop table person")
+    }
+
+    // Create the table
+    _, err = db.Exec(createperson)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Insert a bunch of records. 
+    for i := 0; i < 10000; i++ {
+        log.Print(i, " ")
+        // This is where it will fail, if it does.
+		err := maxProcsInsert(db, randomName(), randomEmail())
+        if err != nil {
+            log.Fatal(err, "\nProblem inserting person")
+        }
+    }
+}
+
+func randomName() string {
+    alphas := `abcdefghijklmnopqrstuvwxyz`
+    name := ``
+    for i := 0; i < 10; i++ {
+        name += string(alphas[rand.Int()%26])
+    }
+    return name
+}
+
+func randomEmail() string {
+    return randomName() + "@" + randomName() + ".com"
+}
+
+func maxProcsInsert(db *sql.DB, name, email string) error {
+    log.Println("Inserting: ", name, email)
+	cols := `(name, email) values ($1,$2) `
+    _, err := db.Exec(`INSERT INTO person `+cols, name, email)
+    if err != nil {
+        return err
+    }
+    return nil
+}
